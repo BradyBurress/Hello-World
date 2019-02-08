f8740a0a947b15fabdbfbff653018702c28023a5
Brady Burress <bradyburress@bradys-macbook-pro.local>
2019-02-08 14:47:53 -0600

    Initial Commit

---
+++ HelloWorldChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldChallenge
+//
+//  Created by Brady Burress on 2/8/19.
+//  Copyright © 2019 Brady Burress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
